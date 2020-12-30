c06fcbf6ef532d07a3c795462f76d267f89b562e
weitson <weitson@outlook.com>
2020-12-30 19:36:44 +0800

    修改PWM的注释

--- src/任务3-6PWM方式输出方波.c
+++ src/任务3-6PWM方式输出方波.c
@@ -1,3 +1,4 @@
+/*任务3-6：从P1.0引脚输出周期为1s，占空比为75%的方波*/
 #include <REG52.H>
 sbit PWM = P1 ^ 0;
 void delay(unsigned int i) //delay(1);表示1ms
@@ -14,8 +15,8 @@ void main()
     while (1)
     {
         PWM = 1;
-        delay(750);
+        delay(750);//高电平持续750ms
         PWM = 0;
-        delay(250);
+        delay(250);//低电平持续250ms
     }
 }
